4893446582a7a4a93f02574f473ed80320b2df53
Teklad <jkinnaird1991@gmail.com>
2018-04-28 21:45:41 -0500

    Add git submodule for INI handling.

---
+++ src/mainwindow.cxx
@@ -0,0 +1,11 @@
+#include "mainwindow.h"
+
+MainWindow::MainWindow(int sizeX, int sizeY) : Fl_Window(sizeX, sizeY)
+{
+
+}
+
+MainWindow::~MainWindow()
+{
+
+}
---
+++ src/mainwindow.h
@@ -0,0 +1,7 @@
+#include <FL/Fl_Window.H>
+
+class MainWindow : public Fl_Window {
+    public:
+        MainWindow(int sizeX, int sizeY);
+        ~MainWindow();
+};
